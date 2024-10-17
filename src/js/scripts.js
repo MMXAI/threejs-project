c66e6b42998de3bd407eb53ea70d41151e8a8138
MohammadX <shahrabadi.msv@gmail.com>
2024-10-18 00:25:01 +0330

    Added Plane and Sphere

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -10,7 +10,7 @@ document.body.appendChild(renderer.domElement);
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
-  75,
+  45,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
@@ -22,14 +22,33 @@ const axesHelper = new THREE.AxesHelper(3);
 
 scene.add(axesHelper);
 
-camera.position.set(0, 0, 5);
+camera.position.set(-10, 30, 30);
 orbit.update();
 
 const boxGeometry = new THREE.BoxGeometry();
 const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
-
 scene.add(box);
+
+const planeGeometry = new THREE.PlaneGeometry(30, 30);
+const planeMaterial = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
+  side: THREE.DoubleSide,
+});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(plane);
+plane.rotation.x = -0.5 * Math.PI;
+
+const gridHelper = new THREE.GridHelper(30, 10);
+scene.add(gridHelper);
+
+const sphereGeometry = new THREE.SphereGeometry(4, 100, 100);
+const sphereMaterial = new THREE.MeshBasicMaterial({
+  color: 0x0000ff,
+  wireframe: true,
+});
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+scene.add(sphere);
 
 function animate() {
   box.rotation.x += 0.01;
