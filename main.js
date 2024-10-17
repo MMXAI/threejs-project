0d033c9b94405f20f71cda0d3979e35131a7db81
MohammadX <shahrabadi.msv@gmail.com>
2024-10-17 13:31:06 +0330

    Drew a line (LineBasicMaterial, Vector3, BufferGeometry)

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 
+//Creating renderer, camera, and scene
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -14,3 +15,26 @@ camera.position.set(0, 0, 100);
 camera.lookAt(0, 0, 0);
 
 const scene = new THREE.Scene();
+
+//Next thing we will do is define a material.
+//For lines we have to use LineBasicMaterial or LineDashedMaterial.
+
+//create a blue LineBasicMaterial
+const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+
+//After material we will need a geometry with some vertices:
+const points = [];
+points.push(new THREE.Vector3(-10, 0, 0));
+points.push(new THREE.Vector3(0, 10, 0));
+points.push(new THREE.Vector3(10, 0, 0));
+
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+//Now that we have points for two lines and a material,
+//we can put them together to form a line.
+
+const line = new THREE.Line(geometry, material);
+
+//All that's left is to add it to the scene and call render.
+scene.add(line);
+renderer.render(scene, camera);
